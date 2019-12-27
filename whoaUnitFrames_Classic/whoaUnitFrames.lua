20b4c4b53d72f40ef18c661e7c624c05da6066a9
Marcus Melvinson <knubbbe@gmail.com>
2019-12-27 19:10:56 +0100

    Updates

--- whoaUnitFrames_Classic/whoaUnitFrames.lua
+++ whoaUnitFrames_Classic/whoaUnitFrames.lua
@@ -13,7 +13,10 @@ local deadText = DEAD;
 --	Blue shamans instead of pink.
 function blueShamans ()
 	if (cfg.blueShamans == true) then
-		RAID_CLASS_COLORS["SHAMAN"] = CreateColor(0.0, 0.44, 0.87);
+		RAID_CLASS_COLORS['SHAMAN']["r"] = 0.0
+		RAID_CLASS_COLORS['SHAMAN']["g"] = 0.44
+		RAID_CLASS_COLORS['SHAMAN']["b"] = 0.87
+		RAID_CLASS_COLORS['SHAMAN']["colorStr"] = "ff0070de"
 	end
 end
 		
